dcfd3e16623db9b631e5e39641fd06a4e8138567
Segger <saintdane@gmail.com>
2023-12-09 16:59:20 -0500

    Delete clipMacWin.bat

--- clipMacWin.bat
+++
@@ -1,6 +0,0 @@
-@REM // ╭───────────────────────────────────────────────────────────────────────────╮
-@REM // │                            Windows clipMAC                                │
-@REM // │  Gets system mac address and places the result in the windows clipboard.  │
-@REM // ╰───────────────────────────────────────────────────────────────────────────╯
-@echo off
-powershell -Command "Get-NetAdapter | Where-Object { $_.Status -eq 'Up' } | ForEach-Object { $_.MacAddress -replace '-', ':' | Set-Clipboard }"
